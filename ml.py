0fa641d0939617f6547c8d2f3d9f2b5b17340fbe
yaowenqiang <yaowenqiang111@163.com>
2024-03-19 00:25:57 +0800

    add makefile

--- ml.py
+++ ml.py
@@ -1,8 +1,9 @@
 import pandas
+import warnings
+from icecream import ic
 filename = 'forestfires.csv'
 names = ['X', 'Y', 'month', 'day', 'FFMC', 'DMC', 'DC', 'ISI', 'temp', 'RH', 'wind', 'rain', 'area']
 df = pandas.read_csv(filename, names=names)
-print(df)
-
+ic(pandas.isnull(df).sum())
 
 
